433c59b17450184a37bf9b984f9d24d2fb1fe102
vasa97 <49680101+vasa97@users.noreply.github.com>
2019-08-31 10:04:06 +0430

    profile

--- app/src/main/java/i/part/app/course/todo/MainActivity.kt
+++ app/src/main/java/i/part/app/course/todo/MainActivity.kt
@@ -1,12 +1,20 @@
 package i.part.app.course.todo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(
+            R.id.fragment_container, Profile()).commit()
     }
 }
